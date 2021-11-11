0ea2f959c06c160177f449be52009152de24a176
jamalgithub <jelmoubarik@gmail.com>
2021-11-11 21:07:15 +0100

    finished Lecture4

--- src/test/java/lectures/Lecture4.java
+++ src/test/java/lectures/Lecture4.java
@@ -6,20 +6,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class Lecture4 {
 
   @Test
-  public void distinct() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
+  public void distinct() {
+    final List<Integer> numbers = ImmutableList
+        .of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+    List<Integer> numbersDistinct = numbers.stream().distinct().collect(Collectors.toList());
+    System.out.println(numbersDistinct);
+    assertThat(numbersDistinct).isNotEmpty();
   }
 
   @Test
-  public void distinctWithSet() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
+  public void distinctWithSet() {
+    final List<Integer> numbers = ImmutableList
+        .of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+    Set<Integer> numbersDistinct = numbers.stream().collect(Collectors.toSet());
+    System.out.println(numbersDistinct);
+    assertThat(numbersDistinct).isNotEmpty();
   }
 }
