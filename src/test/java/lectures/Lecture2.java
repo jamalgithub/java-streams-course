68e82935c82d56a18c611d98fd33e194c1b31937
jamalgithub <jelmoubarik@gmail.com>
2021-11-11 15:04:40 +0100

    finished Lecture2

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,20 +10,27 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
-
+    System.out.println("exclusive");
+    IntStream.range(0, 10).forEach(System.out::println);
+    System.out.println("inclusive");
+    IntStream.rangeClosed(0, 10).forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
-
+    System.out.println("exclusive");
+    IntStream.range(0, people.size()).mapToObj(i -> people.get(i)).forEach(System.out::println);
+    System.out.println("inclusive");
+    IntStream.rangeClosed(0, people.size()).mapToObj(i -> people.get(i))
+        .forEach(System.out::println);
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    IntStream.iterate(0, operand -> operand + 1)
+        .filter(number -> number % 2 == 0)
+        .limit(20)
+        .forEach(System.out::println);
   }
 }
