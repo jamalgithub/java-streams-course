4eddd413ab6a674d13e1d36ea8a7f9711abfafe0
jamalgithub <jelmoubarik@gmail.com>
2021-11-11 22:30:28 +0100

    finished Lecture5

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -8,7 +8,7 @@ import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
@@ -18,20 +18,28 @@ public class Lecture5 {
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
-
+    List<Car> carList = cars.stream().filter(car -> car.getPrice() < 10000)
+        .collect(Collectors.toList());
+    carList.stream().forEach(System.out::println);
+    assertThat(carList).isNotEmpty();
   }
 
   @Test
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
-
+    List<PersonDTO> personDTOS = people.stream().map(PersonDTO::map).collect(Collectors.toList());
+    personDTOS.stream().forEach(System.out::println);
+    assertThat(personDTOS).isNotEmpty();
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
-
+    ImmutableList<Car> cars = MockData.getCars();
+    double average = cars.stream().mapToDouble(car -> car.getPrice()).average().orElse(0);
+    System.out.println(average);
+    assertThat(average).isNotZero();
   }
 
   @Test
