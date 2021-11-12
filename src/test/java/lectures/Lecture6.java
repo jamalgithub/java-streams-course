f372ac01662a93fcc9fdcdc495184b59cdf937b0
jamalgithub <jelmoubarik@gmail.com>
2021-11-12 21:56:35 +0100

    finished Lecture6

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -1,7 +1,8 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Arrays;
 import java.util.function.Predicate;
 import org.junit.Test;
@@ -11,15 +12,18 @@ public class Lecture6 {
   final Predicate<Integer> numbersLessThan10 = n -> n > 5 && n < 10;
 
   @Test
-  public void findAny() throws Exception {
+  public void findAny() {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    int any = Arrays.stream(numbers).filter(numbersLessThan10).findAny().orElse(0);
+    assertThat(any).isNotEqualTo(10);
   }
 
   @Test
-  public void findFirst() throws Exception {
+  public void findFirst() {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    Integer first = Arrays.stream(numbers).filter(numbersLessThan10).findFirst().get();
+    System.out.println(first);
+    assertThat(first).isEqualTo(6);
   }
 }
 
