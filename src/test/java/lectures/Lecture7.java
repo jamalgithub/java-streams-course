766966260690bb2ef3782503cfcb9440a0c45df2
jamalgithub <jelmoubarik@gmail.com>
2021-11-12 23:15:55 +0100

    finished Lecture7

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -4,11 +4,9 @@ package lectures;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import beans.Car;
-import com.google.common.collect.ImmutableList;
 import java.math.BigDecimal;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
@@ -16,24 +14,37 @@ public class Lecture7 {
 
   @Test
   public void count() throws Exception {
-
+    long count = MockData.getPeople().stream()
+        .filter(p -> p.getGender().equalsIgnoreCase("female")).count();
+    System.out.println(count);
+    assertThat(count).isNotZero();
   }
 
   @Test
   public void min() throws Exception {
-
+    List<Car> cars = MockData.getCars();
+    double min = cars.stream().filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice).min().getAsDouble();
+    System.out.println(min);
+    assertThat(min).isEqualTo(6600.59);
   }
 
   @Test
   public void max() throws Exception {
-
+    List<Car> cars = MockData.getCars();
+    double min = cars.stream().filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice).max().getAsDouble();
+    System.out.println(min);
+    assertThat(min).isEqualTo(99677.14);
   }
 
 
   @Test
   public void average() throws Exception {
     List<Car> cars = MockData.getCars();
-
+    double average = cars.stream().mapToDouble(Car::getPrice).average().orElse(0);
+    System.out.println(average);
+    assertThat(average).isEqualTo(52693.19979);
   }
 
   @Test
@@ -45,7 +56,7 @@ public class Lecture7 {
     BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
     System.out.println(sum);
     System.out.println(bigDecimalSum);
-
+    assertThat(sum).isEqualTo(5.269319979E7);
   }
 
   @Test
