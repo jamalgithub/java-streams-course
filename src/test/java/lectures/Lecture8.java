359b2033838074a17fcd21e18c909653c339c4c1
jamalgithub <jelmoubarik@gmail.com>
2021-11-13 23:39:50 +0100

    finished Lecture8

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -1,12 +1,13 @@
 package lectures;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import beans.Car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.assertj.core.util.Lists;
@@ -16,7 +17,13 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
-
+    Map<String, List<Car>> grouping = MockData.getCars().stream()
+        .collect(Collectors.groupingBy(Car::getMake));
+    grouping.forEach((make, cars) -> {
+      System.out.println("******** " + make + " *********");
+      cars.forEach(System.out::println);
+    });
+    assertThat(grouping).isNotEmpty();
   }
 
   @Test
@@ -33,8 +40,10 @@ public class Lecture8 {
             "Alex",
             "Alex"
         );
-
-
+    Map<String, Long> counting = names.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    counting.forEach((name, count) -> System.out.println(name + " : " + count));
+    assertThat(counting).isNotEmpty();
   }
 
 }
