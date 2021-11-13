21de4a8a8aebe9057579cb15395f6f1a0a5e9c01
jamalgithub <jelmoubarik@gmail.com>
2021-11-14 00:08:57 +0100

    finished Lecture10

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -1,8 +1,11 @@
 package lectures;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Before;
@@ -22,15 +25,24 @@ public class Lecture10 {
   }
 
   @Test
-  public void withoutFlatMap() throws Exception {
+  public void withoutFlatMap() {
 //    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+    List<String> names = org.assertj.core.util.Lists.newArrayList();
+    for (List<String> listOfNames : arrayListOfNames) {
+      Collections.addAll(listOfNames);
+    }
+    System.out.println(names);
+    assertThat(names).isNotEmpty();
 
   }
 
   @Test
   public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
-
+    List<String> flatMap = arrayListOfNames.stream().flatMap(List::stream)
+        .collect(Collectors.toList());
+    System.out.println(flatMap);
+    assertThat(flatMap).isNotEmpty();
   }
 
 }
