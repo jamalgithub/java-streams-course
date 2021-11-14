faecd6e751735ab0373a5095d88ed0d917f74611
jamalgithub <jelmoubarik@gmail.com>
2021-11-14 23:43:21 +0100

    finished Lecture11

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,31 @@
 package lectures;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.stream.Collectors;
-import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture11 {
 
   @Test
-  public void joiningStrings() throws Exception {
+  public void joiningStrings() {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String join = "";
+    for (String name : names) {
+      join += name + ", ";
+    }
+    join = join.substring(0, join.length() - 2);
+    System.out.println(join);
+    assertThat(join).isNotEmpty();
   }
 
   @Test
-  public void joiningStringsWithStream() throws Exception {
+  public void joiningStringsWithStream() {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String join = names.stream().collect(Collectors.joining(","));
+    System.out.println(join);
+    assertThat(join).isEqualTo("anna,john,marcos,helena,yasmin");
   }
 }
